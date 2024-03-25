acdf5ff8e2e933f9abaedd7dc2f6b1c9b7a5bb55
sangbin <sbpark0611@kaist.ac.kr>
2024-03-25 21:52:08 +0900

    1

---
+++ scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J radial_arm_maze_1
+#SBATCH -N 1
+#SBATCH -n 1
+#sbatch --gpus=2
+#SBATCH -p jepyc
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+#SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/radial_arm_maze
+
+__conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$__conda_setup"
+unset __conda_setup
+conda activate pydreamer
+export WANDB_MODE=offline
+export PYTHONPATH='.'
+
+HYDRA_FULL_ERROR=1 python train_epn_ram.py
