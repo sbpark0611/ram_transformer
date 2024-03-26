1dd7a6f2864c8a3fdab0a00c05e6c11fc638c083
sangbin <sbpark0611@kaist.ac.kr>
2024-03-26 16:41:20 +0900

    1

--- scripts.sh
+++ scripts.sh
@@ -8,13 +8,13 @@
 #SBATCH -e %x.err
 #SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/radial_arm_maze
 
-module load anaconda3/2020.11
-module load cudatoolkit/10.2
+module load anaconda/23.09.0
+module load cudatoolkit/11.7
 
 __conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 eval "$__conda_setup"
 unset __conda_setup
-conda activate pydreamer
+conda activate ram
 export WANDB_MODE=offline
 export PYTHONPATH='.'
 
