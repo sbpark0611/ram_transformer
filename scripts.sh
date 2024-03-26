7452a0383f1c0a32ed4c4dc4770c75de907cad1a
sangbin <sbpark0611@kaist.ac.kr>
2024-03-26 15:42:17 +0900

    1

--- scripts.sh
+++ scripts.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#SBATCH -J radial_arm_maze_1
+#SBATCH -J radial_arm_maze
 #SBATCH -N 1
 #SBATCH -n 1
-#sbatch --gpus=2
+#SBATCH -c 1
+#sbatch --gpus=1
 #SBATCH -p jepyc
 #SBATCH -o %x.out
 #SBATCH -e %x.err
 #SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/radial_arm_maze
+
+source $MODULESHOME/init/sh
+module list
+module unload cudatoolkit
+module load anaconda3/2020.11
+module load cudatoolkit/10.2
 
 __conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 eval "$__conda_setup"
@@ -16,3 +23,9 @@ export WANDB_MODE=offline
 export PYTHONPATH='.'
 
 HYDRA_FULL_ERROR=1 python train_epn_ram.py
+
+
+
+
+
+
