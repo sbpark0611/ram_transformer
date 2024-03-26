af13b39556a26cc4890b886049fbd001fdaad24a
sangbin <sbpark0611@kaist.ac.kr>
2024-03-26 17:20:10 +0900

    1

--- scripts.sh
+++ scripts.sh
@@ -11,7 +11,7 @@
 module load anaconda/23.09.0
 module load cudatoolkit/11.7
 
-__conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/opt/ibs_lib/apps/anaconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 eval "$__conda_setup"
 unset __conda_setup
 conda activate ram
