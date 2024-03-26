4600770b935b2f9e23fbf428b32b8e3005815c4a
sangbin <sbpark0611@kaist.ac.kr>
2024-03-26 18:24:05 +0900

    1

--- scripts.sh
+++ scripts.sh
@@ -14,6 +14,9 @@ module load cudatoolkit/11.7
 __conda_setup="$('/opt/ibs_lib/apps/anaconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 eval "$__conda_setup"
 unset __conda_setup
+
+conda info --envs
+
 conda activate ram2
 export WANDB_MODE=offline
 export PYTHONPATH='.'
