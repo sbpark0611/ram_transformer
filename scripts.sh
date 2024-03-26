55962431927e236cfea46f5151f15c78536f9def
sangbin <sbpark0611@kaist.ac.kr>
2024-03-26 16:06:20 +0900

    1

--- scripts.sh
+++ scripts.sh
@@ -8,6 +8,9 @@
 #SBATCH -e %x.err
 #SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/radial_arm_maze
 
+module load anaconda3/2020.11
+module load cudatoolkit/10.2
+
 __conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 eval "$__conda_setup"
 unset __conda_setup
