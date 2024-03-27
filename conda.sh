c50bd157649b465c61aaf61d7309c0632aef268a
sangbin <sbpark0611@kaist.ac.kr>
2024-03-27 22:49:43 +0900

    1

---
+++ conda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J radial_arm_maze
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH -p normal
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+#SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/radial_arm_maze
+
+module load anaconda/23.09.0
+module load cudatoolkit/11.7
+
+conda create -n ram
+y
